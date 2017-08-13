d49f8f0454ba5a1739bdd55cdc65ddc426f11cfd
Alex Gaudiosi <alex.gaudiosi@simplybusiness.co.uk>
2017-08-13 14:43:08 +0100

    feat: basic bookshelf component with styled books rendered

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import './App.css';
 
 class BooksApp extends React.Component {
   state = {
+    books: [],
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: true
+    showSearchPage: false
   }
 
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
-
-    });
+      this.setState({ books });
+    })
   }
 
   render() {
@@ -53,6 +54,7 @@ class BooksApp extends React.Component {
               <div>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
+                  <ListBooks books={this.state.books} />
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                       <li>
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -2,8 +2,29 @@ import React, { Component } from 'react';
 
 class ListBooks extends Component {
   render() {
-    let showingBooks;
 
+    const { books } = this.props;
+
+    return (
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          { books.map((book) => (
+            <li>
+              <div className="book">
+                <div className="book-top">
+                  <div
+                    className="book-cover"
+                    style={{ backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}
+                  />
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
+    )
 
   }
 }
